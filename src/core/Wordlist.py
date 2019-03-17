1498385f843e83b84a056f0711859a2b37967733
Vladislav Nechakhin <nechakhin@gmail.com>
2019-03-17 18:11:45 +0700

    Refactor

--- src/core/Wordlist.py
+++ src/core/Wordlist.py
@@ -6,35 +6,35 @@ class Wordlist:
     pattern_symbol = '%'
 
     def __init__(self, wordlist_path: str, extensions):
-        self.__wordlist_path = wordlist_path
-        self.__extensions = []
-        self.__set_extensions(extensions)
+        self._wordlist_path = wordlist_path
+        self._extensions = []
+        self._set_extensions(extensions)
 
-    def __set_extensions(self, extensions):
+    def _set_extensions(self, extensions):
         # append extensions from file / list from cli
         if isinstance(extensions, list):
-            self.__extensions.extend(extensions)
+            self._extensions.extend(extensions)
         elif isinstance(extensions, str):
             with open(extensions, 'r') as extensions_file:
                 for line in extensions_file:
-                    if self.__is_comment(line):
+                    if self._is_comment(line):
                         continue
-                    self.__extensions.append(line)
+                    self._extensions.append(line)
 
-    def __is_comment(self, line: str):
+    def _is_comment(self, line: str):
         return line.lstrip().startswith('#')
 
     @thread_safe_generator
     def __iter__(self):
-        with open(self.__wordlist_path, 'r') as wordlist_file:
+        with open(self._wordlist_path, 'r') as wordlist_file:
             for line in wordlist_file:
                 # Skip comments line
-                if self.__is_comment(line):
+                if self._is_comment(line):
                     continue
 
                 sample = line.rstrip()
 
-                for ext in self.__extensions:
+                for ext in self._extensions:
                     if self.pattern_symbol in ext:
                         # If extension contains template, will replace the pattern symbol by sample
                         yield ext.replace(self.pattern_symbol, sample, 1)
