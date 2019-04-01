b7b9892c1063742b2bff793b15ec6e491fc9a3e8
Vladislav Nechakhin <nechakhin@gmail.com>
2019-04-01 22:33:36 +0700

    Add appending extensions from cli and file

--- src/core/Wordlist.py
+++ src/core/Wordlist.py
@@ -1,3 +1,5 @@
+from os.path import isfile
+
 from src.utils.GeneratorUtils import thread_safe_generator
 
 
@@ -5,21 +7,25 @@ class Wordlist:
 
     pattern_symbol = '%'
 
-    def __init__(self, wordlist_path: str, extensions):
+    def __init__(self, wordlist_path: str, extensions: list, extensions_file: str):
         self._wordlist_path = wordlist_path
         self._extensions = []
-        self._set_extensions(extensions)
+        self._set_extensions(extensions, extensions_file)
 
-    def _set_extensions(self, extensions):
-        # append extensions from file / list from cli
-        if isinstance(extensions, list):
-            self._extensions.extend(extensions)
-        elif isinstance(extensions, str):
-            with open(extensions, 'r') as extensions_file:
-                for line in extensions_file:
+    def _set_extensions(self, extensions: list, extensions_file: str):
+        if extensions_file is not None:
+            if not isfile(extensions_file):
+                # todo('raise exception')
+                return
+            with open(extensions_file, 'r') as file:
+                for line in file:
                     if self._is_comment(line):
                         continue
                     self._extensions.append(line)
+
+        for extension in extensions:
+            if extension not in self._extensions:
+                self._extensions.append(extension)
 
     def _is_comment(self, line: str):
         return line.lstrip().startswith('#')
