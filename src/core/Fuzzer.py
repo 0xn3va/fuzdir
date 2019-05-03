4ae91d06d62f9e83593d5934c72a15d43f97a7a2
Vladislav Nechakhin <nechakhin@gmail.com>
2019-05-03 21:51:57 +0700

    Refactoring & add ctrl+c handling

--- src/core/Fuzzer.py
+++ src/core/Fuzzer.py
@@ -15,30 +15,37 @@ class Fuzzer:
         self._requests = requests
         self._filter = filter
         self.threads = threads
-        self._lock = threading.Lock()
+        self._shutdown_lock = threading.Lock()
+        self._index_lock = threading.Lock()
         self._shutdown = False
         self._paths = None
         self._index = 0
 
     @property
     def _shutdown(self):
-        with self._lock:
+        with self._shutdown_lock:
             return self._cancel
 
     @_shutdown.setter
     def _shutdown(self, shutdown: bool):
-        with self._lock:
+        with self._shutdown_lock:
             self._cancel = shutdown
+
+    def _index_increment(self):
+        with self._index_lock:
+            self._index += 1
+            return self._index
 
     def _request(self, output: Output):
         try:
             while True:
+                try:
+                    path = next(self._paths)
+                except StopIteration:
+                    break
+                message = self._requests.request(path=path)
                 if self._shutdown:
                     break
-
-                path = next(self._paths)
-                message = self._requests.request(path=path)
-
                 if message.type == MessageType.error:
                     output.print_error_log(message.body)
                 else:
@@ -46,23 +53,19 @@ class Fuzzer:
                     if self._filter.inspect(response):
                         output.print_response(response)
 
-                with self._lock:
-                    self._index += 1
-                    output.print_progress_bar(float(self._index) / float(self._wordlist_len) * 100)
+                output.print_progress_bar(float(self._index_increment()) / float(self._wordlist_len) * 100)
         except RequestError as e:
             self._shutdown = True
             output.print_error(str(e))
             return
-        except StopIteration:
-            return
 
     def start(self, output: Output):
+        threads = set()
         try:
             self._shutdown = False
             self._paths = iter(self._wordlist)
             self._index = 0
 
-            threads = set()
             for _ in range(self.threads):
                 thread = threading.Thread(target=self._request, args=(output,))
                 threads.add(thread)
@@ -71,6 +74,12 @@ class Fuzzer:
             for thread in threads:
                 thread.join()
         except KeyboardInterrupt:
-            output.print_error('KeyboardInterrupt')
             self._shutdown = True
-            return
+            output.print_error('KeyboardInterrupt')
+        finally:
+            while any(thread.is_alive() for thread in threads):
+                try:
+                    for thread in threads:
+                        thread.join()
+                except KeyboardInterrupt:
+                    continue
