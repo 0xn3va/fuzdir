30c5fc13e663b5d883a8a9bc5b935b1338e66c02
Vladislav Nechakhin <nechakhin@gmail.com>
2019-05-25 23:44:12 +0700

    Refactoring

--- src/core/Fuzzer.py
+++ src/core/Fuzzer.py
@@ -16,54 +16,26 @@ class Fuzzer:
         self._requests = requests
         self._filter = filter
         self.threads = threads
-        self._shutdown_lock = threading.Lock()
+        self._is_cancel_lock = threading.Lock()
         self._index_lock = threading.Lock()
-        self._shutdown = False
+        self._is_cancel = False
         self._paths = None
         self._index = 0
 
     @property
-    def _shutdown(self):
-        with self._shutdown_lock:
-            return self._cancel
+    def _is_cancel(self):
+        with self._is_cancel_lock:
+            return self._status
 
-    @_shutdown.setter
-    def _shutdown(self, shutdown: bool):
-        with self._shutdown_lock:
-            self._cancel = shutdown
-
-    def _index_increment(self):
-        with self._index_lock:
-            self._index += 1
-            return self._index
-
-    def _request(self, output: Output):
-        try:
-            while True:
-                try:
-                    path = next(self._paths)
-                except StopIteration:
-                    break
-                message = self._requests.request(path=path)
-                if self._shutdown:
-                    break
-                if message.type == ResponseType.error:
-                    output.print(ResponseType.log, message.body)
-                else:
-                    response = message.body
-                    if self._filter.inspect(response):
-                        output.print_response(response)
-
-                output.progress_bar(float(self._index_increment()) / float(self._wordlist_len) * 100)
-        except RequestError as e:
-            self._shutdown = True
-            output.print(MessageType.error, str(e))
-            return
+    @_is_cancel.setter
+    def _is_cancel(self, status: bool):
+        with self._is_cancel_lock:
+            self._status = status
 
     def start(self, output: Output):
         threads = set()
         try:
-            self._shutdown = False
+            self._is_cancel = False
             self._paths = iter(self._wordlist)
             self._index = 0
 
@@ -75,8 +47,7 @@ class Fuzzer:
             for thread in threads:
                 thread.join()
         except KeyboardInterrupt:
-            self._shutdown = True
-            output.print(MessageType.error, 'KeyboardInterrupt')
+            self._cancel(output, 'KeyboardInterrupt')
         finally:
             while any(thread.is_alive() for thread in threads):
                 try:
@@ -84,3 +55,37 @@ class Fuzzer:
                         thread.join()
                 except KeyboardInterrupt:
                     continue
+
+    def _request(self, output: Output):
+        try:
+            while True:
+                try:
+                    path = next(self._paths)
+                except StopIteration:
+                    break
+                message = self._requests.request(path=path)
+                if self._canceled():
+                    break
+                if message.type == ResponseType.error:
+                    output.print(ResponseType.log, message.body)
+                else:
+                    response = message.body
+                    if self._filter.inspect(response):
+                        output.print_response(response)
+
+                output.progress_bar(float(self._index_increment()) / float(self._wordlist_len) * 100)
+        except RequestError as e:
+            self._cancel(output, str(e))
+
+    def _canceled(self):
+        return self._is_cancel
+
+    def _cancel(self, output: Output, e: str):
+        if not self._canceled():
+            self._is_cancel = True
+            output.print(MessageType.error, e)
+
+    def _index_increment(self):
+        with self._index_lock:
+            self._index += 1
+            return self._index
