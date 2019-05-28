0837302bdf2ab1ebd7d11c779e2cf6bbc9e46a06
Vladislav Nechakhin <nechakhin@gmail.com>
2019-05-28 22:14:51 +0700

    Fix dirname receiving

--- src/utils/FileUtils.py
+++ src/utils/FileUtils.py
@@ -7,16 +7,20 @@ class FileUtils:
     def dir_exist(path: str) -> bool:
         if not isinstance(path, str):
             return False
-        return os.path.isdir(os.path.dirname(path))
+        return os.path.isdir(FileUtils.dirname(path))
 
     @staticmethod
     def is_writable(path: str) -> bool:
         if not isinstance(path, str):
             return False
-        return os.access(os.path.dirname(path), os.W_OK) and not os.path.isdir(path)
+        return os.access(FileUtils.dirname(path), os.W_OK) and not os.path.isdir(path)
 
     @staticmethod
     def is_readable(path: str) -> bool:
         if not isinstance(path, str):
             return False
-        return os.path.isfile(path) and os.access(os.path.dirname(path), os.R_OK)
+        return os.path.isfile(path) and os.access(FileUtils.dirname(path), os.R_OK)
+
+    @staticmethod
+    def dirname(path: str) -> str:
+        return os.path.dirname(os.path.abspath(path))
