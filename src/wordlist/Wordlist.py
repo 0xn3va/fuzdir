bf59d0181e2c7fde54055045e1cfbf866fcc0b6b
Vladislav Nechakhin <nechakhin@gmail.com>
2019-07-16 20:53:22 +0700

    Refactoring & replace threadsafe decorator & check ext begins with .

--- src/wordlist/Wordlist.py
+++ src/wordlist/Wordlist.py
@@ -1,59 +1,54 @@
+import threading
+
 from src.utils.FileUtils import FileUtils
-from src.utils.GeneratorUtils import thread_safe_generator
 from src.wordlist.Encoding import Encoding
 
 
 class Wordlist:
-    pattern_symbol = '%'
+    _pattern_symbol = '%'
     _comment_symbol = b'#'
+    _filename_format = '%s.%s'
 
     def __init__(self, wordlist_path: str, extensions: list, extensions_file: str):
+        self._lock = threading.Lock()
         if not FileUtils.is_readable(wordlist_path):
-            raise FileExistsError('The wordlist file does not exist or access denied')
+            raise IOError('The wordlist file does not exist or access denied')
         self._wordlist_path = wordlist_path
         self._wordlist_size = sum(1 for _ in self._read_file(self._wordlist_path))
+        # extensions from cli and file
         self._extensions = []
-        self._set_extensions(extensions, extensions_file)
-
-    def _set_extensions(self, extensions: list, extensions_file: str):
         if extensions_file is not None:
             if not FileUtils.is_readable(extensions_file):
-                raise FileExistsError('The extensions file does not exist or access denied')
+                raise IOError('The extensions file does not exist or access denied')
             self._extensions.extend(self._read_file(extensions_file))
-
         for extension in extensions:
             if extension not in self._extensions:
                 self._extensions.append(extension)
 
-    def _read_file(self, path):
-        with open(path, 'rb') as file:
-            for line in file:
-                line = line.strip()
-                # Skip comments line
-                if not line or self._is_comment(line):
-                    continue
-                yield Encoding.decode(line)
-
-    def _is_comment(self, line: bytes):
-        try:
-            return line[0] == self._comment_symbol
-        except IndexError:
-            return False
-
     @property
-    def extensions(self):
+    def extensions(self) -> list:
         return self._extensions
 
     def __len__(self):
         return self._wordlist_size + self._wordlist_size * len(self._extensions)
 
-    @thread_safe_generator
     def __iter__(self) -> str:
-        for sample in self._read_file(self._wordlist_path):
-            for ext in self._extensions:
-                if self.pattern_symbol in ext:
-                    yield ext.replace(self.pattern_symbol, sample, 1)
-                else:
-                    yield '%s.%s' % (sample, ext,)
+        with self._lock:
+            for sample in self._read_file(self._wordlist_path):
+                for extension in self._extensions:
+                    if self._pattern_symbol in extension:
+                        yield extension.replace(self._pattern_symbol, sample, 1)
+                    elif extension.startswith('.'):
+                        yield sample + extension
+                    else:
+                        yield self._filename_format % (sample, extension,)
+                yield sample
 
-            yield sample
+    def _read_file(self, path: str) -> str:
+        with open(path, 'rb') as file:
+            for line in file:
+                line = line.strip()
+                # Skip comments line
+                if not line or line[0] == self._comment_symbol:
+                    continue
+                yield Encoding.decode(line)
