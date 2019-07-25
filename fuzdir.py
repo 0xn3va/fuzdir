793a2d87afe35997926ccbcd7a367516f06c2b00
Vladislav Nechakhin <nechakhin@gmail.com>
2019-07-25 22:28:40 +0700

    Project rename

--- fuzdir.py
+++ fuzdir.py
@@ -5,10 +5,10 @@ import os
 from src.core.Controller import Controller
 
 
-def dirb():
+def fuzdir():
     controller = Controller(root_path=os.path.abspath(os.path.dirname(__file__)))
     controller.start()
 
 
 if __name__ == '__main__':
-    dirb()
+    fuzdir()
