8c607d1515bf92e53168b076eab2e9371a868325
Vladislav Nechakhin <nechakhin@gmail.com>
2019-08-10 15:32:32 +0700

    Add thread safe http server & base http request handler

---
+++ test/mocks/httpserver/HTTPRequestHandler.py
@@ -0,0 +1,11 @@
+from http.server import BaseHTTPRequestHandler
+
+
+class HTTPRequestHandler(BaseHTTPRequestHandler):
+    protocol_version = 'HTTP/1.1'
+
+    def _set_headers(self, status_code: int, headers: dict):
+        self.send_response(status_code)
+        for key, value in headers.items():
+            self.send_header(key, value)
+        self.end_headers()
