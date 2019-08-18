8da804ede79bd28878a866b2fa3ce590b85ddf97
Vladislav Nechakhin <nechakhin@gmail.com>
2019-08-18 22:26:03 +0700

    Add url & fixed return from __enter__ method

--- test/mocks/httpserver/http_server_manager.py
+++ test/mocks/httpserver/http_server_manager.py
@@ -4,14 +4,16 @@ from test.mocks.httpserver.threading_http_server import ThreadingHTTPServer
 
 
 class HTTPServerManager:
-    def __init__(self, handler, port: int = 8000):
-        self._http_server = ThreadingHTTPServer(('localhost', port), handler)
+    def __init__(self, handler, port):
+        hostname = 'localhost'
+        self.url = 'http://%s:%d' % (hostname, port,)
+        self._http_server = ThreadingHTTPServer((hostname, port), handler)
 
     def __enter__(self):
         thread = threading.Thread(target=self._http_server.serve_forever)
         thread.daemon = True
         thread.start()
-        return self._http_server
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self._http_server.server_close()
