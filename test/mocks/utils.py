6b41e7ffb169feafb18742acb111a43adfd8f037
Vladislav Nechakhin <nechakhin@gmail.com>
2019-08-27 20:20:01 +0700

    Added random string generation

--- test/mocks/utils.py
+++ test/mocks/utils.py
@@ -1,5 +1,10 @@
 import random
+import string
 
 
 def random_port():
     return random.randint(1024, 65536)
+
+
+def random_string(k=5):
+    return ''.join(random.choices(string.ascii_lowercase, k=k))
