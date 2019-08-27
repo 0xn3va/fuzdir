645c5419918e4c0f0bf25f9a4220ea2f611cef31
Vladislav Nechakhin <nechakhin@gmail.com>
2019-08-27 20:19:28 +0700

    Moved to utils

--- src/network/request/header_names.py
+++
@@ -1,9 +0,0 @@
-
-class HeaderNames:
-    accept_lang = 'Accept-Language'
-    cache_control = 'Cache-Control'
-    cookie = 'Cookie'
-    user_agent = 'User-Agent'
-    host = 'Host'
-    content_length = 'Content-Length'
-    location = 'Location'
--- src/network/request/schemes.py
+++
@@ -1,8 +0,0 @@
-
-class Schemes:
-    allowable = ('http', 'https', )
-    ports = {
-        allowable[0]: 80,
-        allowable[1]: 443
-    }
-    default = allowable[0]
