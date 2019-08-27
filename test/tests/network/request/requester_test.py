1a16fd3d3dbca2c27ccafe6162365078489204c9
Vladislav Nechakhin <nechakhin@gmail.com>
2019-08-27 20:20:14 +0700

    Fixed tests

--- test/tests/network/request/requester_test.py
+++ test/tests/network/request/requester_test.py
@@ -3,21 +3,19 @@ import threading
 import time
 import unittest
 
-from requests.exceptions import RetryError
+import requests
+import urllib3
 
-from src.network.request.request_error import RequestError
+from src.network.request.requester_error import RequesterError
 from src.network.request.requester import Requester
 from src.network.request.throttle.confidence_interval import ConfidenceInterval
-from src.network.response.response_type import ResponseType
 from src.utils.singleton import Singleton
 from test.mocks.httpserver.http_request_handler import HTTPRequestHandler
 from test.mocks.httpserver.http_server_manager import HTTPServerManager
-from test.mocks.utils import random_port
-from test.mocks.wordlist_mock import WordlistMock
+from test.mocks.utils import random_port, random_string
 
 
 class RequesterTest(unittest.TestCase):
-    _wordlist = iter(WordlistMock())
     _stable_iterations = 2 * ConfidenceInterval._window_size
     _changeable_iterations = 4 * ConfidenceInterval._window_size
     _delta = 0.05
@@ -33,17 +31,17 @@ class RequesterTest(unittest.TestCase):
         self.assertEqual(requester.url, full_url_format % (http_scheme, hostname, port,),
                          msg='Check for setting scheme and port by default failed')
         # unknown scheme
-        with self.assertRaises(RequestError, msg='Check for unknown scheme failed'):
+        with self.assertRaises(RequesterError, msg='Check for unknown scheme failed'):
             Requester(url='%s://%s' % (file_scheme, hostname,))
         # incorrect hostname
-        with self.assertRaises(RequestError, msg='Check for incorrect hostname failed'):
+        with self.assertRaises(RequesterError, msg='Check for incorrect hostname failed'):
             Requester(url='%s://%s' % (http_scheme, '',))
         # custom port
         custom_port_url = full_url_format % (http_scheme, hostname, random_port(),)
         requester = Requester(url=custom_port_url)
         self.assertEqual(requester.url, custom_port_url, msg='Check for setting custom port failed')
         # non-root path
-        path = '%s/%s' % (next(self._wordlist), next(self._wordlist),)
+        path = '%s/%s' % (random_string(), random_string(),)
         requester = Requester(url='%s://%s/%s' % (http_scheme, hostname, path,))
         self.assertEqual(requester.url, '%s://%s:%d/%s/' % (http_scheme, hostname, port, path,),
                          msg='Check for fuzzing by non-root path failed')
@@ -54,35 +52,37 @@ class RequesterTest(unittest.TestCase):
                 self._set_headers(status_code=500, headers={'Content-Type': 'text/html', 'Content-Length': 0})
 
         with HTTPServerManager(handler=Handler, port=random_port()) as server:
-            response = Requester(url=server.url).request(next(self._wordlist))
-            self.assertEqual(response.type, ResponseType.error, msg='Check a retry response type failed')
-            self.assertIsInstance(response.body, RetryError, msg='Check a retry response body failed')
+            with self.assertRaises(requests.exceptions.RetryError,  msg='Check a max retries exceeded failed'):
+                _ = Requester(url=server.url).request(random_string())
 
     def test_proxy(self):
         class Handler(HTTPRequestHandler):
             pass
 
         with HTTPServerManager(handler=Handler, port=random_port()) as server:
-            def assert_raises(proxy, msg):
-                with self.assertRaises(RequestError, msg=msg):
-                    requester = Requester(url=server.url, proxy=proxy)
-                    _ = requester.request(next(self._wordlist))
+            with self.assertRaises(requests.exceptions.InvalidProxyURL, msg='Check on invalid proxy url failed'):
+                requester = Requester(url=server.url, proxy='tcp://')
+                _ = requester.request(random_string())
 
-            assert_raises(proxy='tcp://', msg='Check on invalid proxy url failed')
-            assert_raises(proxy='tcp://localhost', msg='Check on not supported proxy scheme failed')
-            assert_raises(proxy='socks5://localhost:%d' % (random_port(),),
-                          msg='Check on connection refused failed')
+            with self.assertRaises(urllib3.exceptions.ProxySchemeUnknown,
+                                   msg='Check on not supported proxy scheme failed'):
+                requester = Requester(url=server.url, proxy='tcp://localhost')
+                _ = requester.request(random_string())
+
+            with self.assertRaises(requests.exceptions.ConnectionError, msg='Check on connection refused failed'):
+                requester = Requester(url=server.url, proxy='socks5://localhost:%d' % (random_port(),))
+                _ = requester.request(random_string())
 
     def test_throttle_fixed(self):
         class Handler(HTTPRequestHandler):
             pass
 
-        throttling_period = random.random()
+        throttling_period = random.uniform(0., 0.25)
         with HTTPServerManager(handler=Handler, port=random_port()) as server:
             requester = Requester(url=server.url, throttling_period=throttling_period)
             prev_time = None
             for _ in range(self._stable_iterations):
-                _ = requester.request(next(self._wordlist))
+                _ = requester.request(random_string())
                 current_time = time.time()
                 if prev_time is not None:
                     self.assertLess(abs(throttling_period - (current_time - prev_time)), self._delta,
@@ -116,13 +116,13 @@ class RequesterTest(unittest.TestCase):
             period = None
             # stable
             for _ in range(self._stable_iterations):
-                _ = requester.request(next(self._wordlist))
+                _ = requester.request(random_string())
                 period = requester._throttle._interval._period
                 self.assertEqual(period, 0., msg='Check for no delay failed')
             # increase
             is_changed = False
             for _ in range(self._changeable_iterations):
-                _ = requester.request(next(self._wordlist))
+                _ = requester.request(random_string())
                 p = requester._throttle._interval._period
                 if p != period:
                     is_changed = True
@@ -132,7 +132,7 @@ class RequesterTest(unittest.TestCase):
             # decreasing
             is_changed = False
             for _ in range(self._changeable_iterations):
-                _ = requester.request(next(self._wordlist))
+                _ = requester.request(random_string())
                 p = requester._throttle._interval._period
                 if p != period:
                     is_changed = True
@@ -141,6 +141,6 @@ class RequesterTest(unittest.TestCase):
             self.assertTrue(is_changed, msg='Check for decreasing delay failed')
             # stable
             for _ in range(self._stable_iterations):
-                _ = requester.request(next(self._wordlist))
+                _ = requester.request(random_string())
                 period = requester._throttle._interval._period
             self.assertEqual(period, 0., msg='Check to return to initial state failed')
