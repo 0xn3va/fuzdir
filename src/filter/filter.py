b75739cda12a5db8be57c2a7e28ceb4fbd22aa59
Vladislav Nechakhin <nechakhin@gmail.com>
2019-08-28 22:14:56 +0700

    Refactored

--- src/filter/filter.py
+++ src/filter/filter.py
@@ -1,51 +1,51 @@
 from requests import Response
 
-from src.filter.conditions.code_condition import CodeCondition
+from src.filter.condition.implement.status_code_condition import StatusCodeCondition
 from src.filter.filter_error import FilterError
-from src.filter.conditions.content_length_condition import ContentLengthCondition
-from src.filter.conditions.grep_condition import GrepCondition
+from src.filter.condition.implement.content_length_condition import ContentLengthCondition
+from src.filter.condition.implement.grep_condition import GrepCondition
 
 
 class Filter:
-    handlers = {
-        'code': CodeCondition,
+    _handlers = {
+        'code': StatusCodeCondition,
         'length': ContentLengthCondition,
         'grep': GrepCondition
     }
-
     _conditions_separator = ';'
     _handler_separator = ':'
     _args_separator = '='
     _ignore_key = 'ignore'
 
-    def __init__(self, conditions: str = ''):
+    def __init__(self, conditions: str):
         self._conditions = []
-
-        if len(conditions) == 0:
+        if not conditions:
             return
 
         for condition in conditions.strip(self._conditions_separator).split(self._conditions_separator):
-            handler, _, condition_args = condition.partition(self._args_separator)
-            if len(condition_args) < 1:
+            head, _, args = condition.partition(self._args_separator)
+            if not args:
                 raise FilterError('Invalid condition: %s' % (condition,))
 
-            head, _, tail = handler.partition(self._handler_separator)
-            ignore = (head == self._ignore_key)
+            key, _, tail = head.partition(self._handler_separator)
+            ignore = (key == self._ignore_key)
             if ignore:
-                handler = tail
+                head = tail
 
-            handler_name, _, handler_args = handler.partition(self._handler_separator)
+            name, _, area = head.partition(self._handler_separator)
 
             try:
-                handler = self.handlers[handler_name]()
-                handler.setup(condition_args, handler_args)
+                handler = self._handlers[name]()
+                handler.setup(args, area)
                 self._conditions.append((ignore, handler))
             except KeyError:
-                raise FilterError('Invalid conditions name: %s' % (handler_name,))
+                raise FilterError('Invalid condition name: %s' % (name,))
 
         self._conditions.sort(key=lambda c: c[-1].priority.value, reverse=True)
 
-    def inspect(self, response: Response):
+    def inspect(self, response: Response) -> bool:
         if response is None:
             return False
-        return True if len(self._conditions) == 0 else all(condition[0] != condition[1].match(response) for condition in self._conditions)
+        if not self._conditions:
+            return True
+        return all(ignore ^ condition.match(response) for ignore, condition in self._conditions)
