545d3cc5646787ed7d5301145b89875db6e4bf81
Vladislav Nechakhin <nechakhin@gmail.com>
2019-08-29 20:32:37 +0700

    Refactored

--- src/core/controller.py
+++ src/core/controller.py
@@ -3,17 +3,17 @@ import os
 import time
 
 from src import output
-from src.argument_parser.argument_parser import ArgumentParser
-from src.argument_parser.argument_parser_error import ArgumentParserError
+from src.argument_parser.argument_manager import ArgumentManager
 from src.core.fuzzer import Fuzzer
+from src.dictionary.extension_list import ExtensionList
+from src.dictionary.word_list import WordList
 from src.utils.file_utils import FileUtils
-from src.wordlist.encoding_error import EncodingError
-from src.wordlist.wordlist import Wordlist
+from src.dictionary.utils.encoding_error import EncodingError
+from src.dictionary.dictionary import Dictionary
 from src.filter.filter import Filter
 from src.filter.filter_error import FilterError
-from src.network.request.request_error import RequestError
+from src.network.request.requester_error import RequesterError
 from src.network.request.requester import Requester
-from src.output.splash_type import SplashType
 
 MAJOR_VERSION = 0
 MINOR_VERSION = 1
@@ -26,6 +26,7 @@ VERSION = {
 
 
 class Controller:
+    _banner_filename = 'banner.txt'
     # logging settings
     _logging_format = '%(asctime)s %(pathname)s, line:%(lineno)d - %(levelname)s - %(message)s'
     _logs_dirname = 'logs'
@@ -33,39 +34,35 @@ class Controller:
     _log_name_format = '%s-%s.%s'
     _log_name_prefix = 'log'
     _log_extension = 'txt'
-    #
-    _banner_filename = 'banner.txt'
 
     def __init__(self, root_path: str):
         try:
-            arg_parser = ArgumentParser()
-            # logging config
-            log_path = self._logging_setup(root_path, arg_parser.verbose)
-            output.config(report_type=arg_parser.report_type, filename=arg_parser.report_path)
-            #
-            wordlist = Wordlist(wordlist_path=arg_parser.wordlist,
-                                extensions=arg_parser.extensions,
-                                extensions_file=arg_parser.extensions_file)
-            requester = Requester(url=arg_parser.url,
-                                  cookie=arg_parser.cookie,
-                                  headers=arg_parser.headers,
-                                  user_agent=arg_parser.user_agent,
-                                  timeout=arg_parser.timeout,
-                                  retries=arg_parser.retry,
-                                  allow_redirects=arg_parser.allow_redirect,
-                                  proxy=arg_parser.proxy,
-                                  throttling_period=arg_parser.throttling_period)
-            filter = Filter(conditions=arg_parser.conditions)
-            self._fuzzer = Fuzzer(wordlist, requester, filter, threads=arg_parser.threads)
-            # format banner and print
+            # print banner
             with open(os.path.join(root_path, self._banner_filename), 'r') as banner_file:
                 banner = banner_file.read()
             banner = banner.format(**VERSION)
-            output.splash(SplashType.banner, banner)
-            output.splash(SplashType.log_path, log_path)
-            output.splash(SplashType.config, wordlist.extensions, self._fuzzer.threads, len(wordlist))
-            output.splash(SplashType.target, requester.url)
-        except (ArgumentParserError, FilterError, NotADirectoryError, IOError, RequestError, EncodingError) as e:
+            output.banner(banner=banner)
+            # parse and prepare arguments
+            argument_manager = ArgumentManager()
+            # logging config
+            log_path = self._logging_setup(root_path, argument_manager.verbose)
+            output.config(report_type=argument_manager.report_type, filename=argument_manager.report_path)
+            # setting main components
+            dictionary = Dictionary(word_list=WordList(path=argument_manager.word_list),
+                                    extension_list=ExtensionList(extensions=argument_manager.extensions,
+                                                                 path=argument_manager.extensions_file))
+            requester = Requester(url=argument_manager.url, user_agent=argument_manager.user_agent,
+                                  cookie=argument_manager.cookie, headers=argument_manager.headers,
+                                  allow_redirect=argument_manager.allow_redirect, timeout=argument_manager.timeout,
+                                  retries=argument_manager.retry, throttling_period=argument_manager.throttling_period,
+                                  proxy=argument_manager.proxy)
+            filter = Filter(conditions=argument_manager.conditions)
+            self._fuzzer = Fuzzer(dictionary=dictionary, requester=requester, filter=filter,
+                                  threads=argument_manager.threads)
+            # print summary
+            output.summary(log_path=log_path, threads=self._fuzzer.threads, dictionary_size=len(dictionary),
+                           target=requester.url)
+        except (IOError, EncodingError, RequesterError, FilterError) as e:
             output.error(str(e))
             exit(0)
 
@@ -73,14 +70,16 @@ class Controller:
         # logs folder settings
         logs_path = os.path.join(root_path, self._logs_dirname)
         if not os.path.isdir(logs_path):
-            raise NotADirectoryError('The logs directory is missing')
+            raise IOError('The logs directory is missing')
         # remove old logs
-        logs = [name for name in os.listdir(logs_path) if name.startswith(self._log_name_prefix) and os.path.isfile(os.path.join(logs_path, name))]
+        logs = [name for name in os.listdir(logs_path)
+                if name.startswith(self._log_name_prefix) and os.path.isfile(os.path.join(logs_path, name))]
         logs.sort()
         for log_name in logs[:max(0, len(logs) - self._logs_capacity)]:
             os.remove(os.path.join(logs_path, log_name))
         # log file settings
-        log_path = os.path.join(logs_path, self._log_name_format % (self._log_name_prefix, time.strftime('%y-%m-%d_%H-%M-%S'), self._log_extension,))
+        log_path = os.path.join(logs_path, self._log_name_format %
+                                (self._log_name_prefix, time.strftime('%y-%m-%d_%H-%M-%S'), self._log_extension,))
         if not FileUtils.is_writable(log_path):
             raise IOError('The log file should be writable')
         # logging settings
