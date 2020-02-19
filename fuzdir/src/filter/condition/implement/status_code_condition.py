de28279058bb7ac5e89e9ce9c64ab327edeceede
Darlene <nechakhin@gmail.com>
2020-02-19 22:52:27 +0700

    Refactored string formatting

--- fuzdir/src/filter/condition/implement/status_code_condition.py
+++ fuzdir/src/filter/condition/implement/status_code_condition.py
@@ -22,15 +22,16 @@ class StatusCodeCondition(Condition):
                 if len(codes) > 1:
                     lower, upper = codes
                     if upper < lower or lower < self._min_code or self._max_code < upper:
-                        raise FilterError('Invalid HTTP status codes range: %s' % (args,))
+                        raise FilterError(f'Invalid HTTP status codes range: {args}')
                     self._codes.append((lower, upper))
                 else:
                     code = codes[0]
                     if code < self._min_code or self._max_code < code:
-                        raise FilterError('Invalid HTTP status code: %s' % (code,))
+                        raise FilterError(f'Invalid HTTP status code: {code}')
                     self._codes.append((codes[0], codes[0]))
         except ValueError:
             raise FilterError('HTTP status code must be a number')
 
     def match(self, response: Response) -> bool:
-        return any(lower <= response.status_code <= upper for lower, upper in self._codes)
+        status_code = response.status_code
+        return any(lower <= status_code <= upper for lower, upper in self._codes)
