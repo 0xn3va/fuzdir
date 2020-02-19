1ae4ed705c3724feee82c691de14c755091e0974
Darlene <nechakhin@gmail.com>
2020-02-19 22:37:18 +0700

    Escaped for loop with enumeration

--- fuzdir/src/dictionary/utils/encoding.py
+++ fuzdir/src/dictionary/utils/encoding.py
@@ -2,13 +2,14 @@ from src.dictionary.utils.encoding_error import EncodingError
 
 
 class Encoding:
-    codecs = ['utf-8', 'cp1251']
+    codecs = ('utf-8', 'cp1251')
 
     @staticmethod
     def decode(line: bytes) -> str:
-        for index, encoding in enumerate(Encoding.codecs):
+        codecs = Encoding.codecs
+        for encoding in codecs:
             try:
                 return line.decode(encoding)
             except UnicodeDecodeError:
-                if index == len(Encoding.codecs) - 1:
+                if encoding == codecs[-1]:
                     raise EncodingError('Unknown file encoding')
